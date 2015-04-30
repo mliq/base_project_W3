daa0a1983f5b9c2b5c73b3793f1897fb51789ba1
mliq <liquori@gmail.com>
2015-04-30 15:14:30 -0500

    Not sure why not displaying to DOM

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-var APP = APP || {};
+var cityData = null, cityHtml = null;
 
 $(document).ready(function(){
 
     $('.get-info-btn').on('click', function(){
-        //console.log(data, "works");
+        if(cityHtml === null){
+            $.get('location.html', function(data){
+                cityHtml = data.slice();
+                $('.more-stuff').append(data);
+            });
+        } else {
+            console.log("You already got the HTML");
+        }
 
+        if(cityData === null){
+            $.get('data.json', function(data){
+                cityData = data;
+                var firstCity = data.locations[0].location;
+                console.log(firstCity);
+                $('.more-stuff').children('.location-name').append("<p>" + firstCity + "</p>");
+            });
+        } else {
+            console.log(cityData.locations[0].population);
+            console.log();
+        }
 
-        $.get('location.html', function(data){
-           //$('.more-stuff').append(data);
-            console.log(data, "works");
-        });
+    });
 
-        $.get('data.json', function(data){
-            console.log("works",data);
-           var firstCity = data.locations[0].location;
-            $('.more-stuff').children('.location-name').append('<p>' + firstCity + "</p>");
-        });
-
+    $("more-stuff").on('click', 'button', function(){
+        $(this).parent().append(cityData.locations[0].population);
     });
 
 });

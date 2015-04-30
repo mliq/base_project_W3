e3892f8361bc6e00578e81b244c9826abd606b31
mliq <liquori@gmail.com>
2015-04-30 14:37:55 -0500

    Stuff works

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var APP = APP || {};
 
-$(document).ready(){
+$(document).ready(function(){
 
-}
+    $('.get-info-btn').on('click', function(){
+        //console.log(data, "works");
+
+
+        $.get('location.html', function(data){
+           //$('.more-stuff').append(data);
+            console.log(data, "works");
+        });
+
+        $.get('data.json', function(data){
+            console.log("works",data);
+           var firstCity = data.locations[0].location;
+            $('.more-stuff').children('.location-name').append('<p>' + firstCity + "</p>");
+        });
+
+    });
+
+});
